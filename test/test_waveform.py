19b57d92b87e91e39d8743ff1a829572921799c7
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2024-11-29 03:35:59 -0800

    fir test_waveform.py

--- test/test_waveform.py
+++ test/test_waveform.py
@@ -27,6 +27,7 @@ These are the unittests for the pycbc.waveform module
 import unittest
 import numpy
 from numpy import sqrt, cos, sin
+from pycbc.filter import overlap
 from pycbc.scheme import CPUScheme
 from pycbc.waveform import get_td_waveform, get_fd_waveform, get_fd_waveform_sequence
 from utils import parse_args_all_schemes, simple_exit
